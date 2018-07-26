5377b7f0e1f35e9b1fdc2a02a62744169cc88328
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 18:35:19 -0300

    Criando a rota para fazer o upload das imagens

--- start/routes.js
+++ start/routes.js
@@ -22,3 +22,6 @@ Route.post('/sessions', 'SessionController.create')
 Route.resource('properties', 'PropertyController')
     .apiOnly()
     .middleware('auth')
+
+Route.post('properties/:id/images', 'ImageController.store')
+    .middleware('auth')
