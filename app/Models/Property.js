c5839f07bdd7f2b772e991fae176d9528cdfe194
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 17:25:59 -0300

    Criando o calculo da distância do usuario para o imóvel

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -1,8 +1,21 @@
 'use strict'
 
 const Model = use('Model')
+const Databese = use('Database')
 
 class Property extends Model {
+    static scopeNearBy(query, latitude, longitude, distance) {
+        const haversine = `(6371 * acos(cos(radians(${latitude}))
+        * cos(radians(latitude))
+        * cos(radians(longitude))
+        - radians(${longitude})
+        + sin(radians(${latitude}))
+        * sin(radians(latitude))))`
+        return query
+            .select('*', Databese.raw(`${haversine} as distance`))
+            .whereRaw(`${haversine} < ${distance}`);
+    }
+
     user() {
         return this.belongsTo('App/Models/User')
     }
