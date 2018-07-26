f455b96f32de2a702c259f1bbaa53dca22d78fd8
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 17:01:53 -0300

    Criando todas ad rotas do imóvel

--- start/routes.js
+++ start/routes.js
@@ -19,4 +19,6 @@ Route.post('/users', 'UserController.create')
 
 Route.post('/sessions', 'SessionController.create')
 
-
+Route.resource('properties', 'PropertyController')
+    .apiOnly()
+    .middleware('auth')
