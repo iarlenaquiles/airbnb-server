8eecbc75ec1071299c53f5e49aebeec67156fceb
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 16:57:36 -0300

    Criando o relacionamento do imóvel com a imagem

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -6,6 +6,10 @@ class Property extends Model {
     user() {
         return this.belongsTo('App/Models/User')
     }
+
+    images() {
+        return this.hasMany('App/Models/Image')
+    }
 }
 
 module.exports = Property
