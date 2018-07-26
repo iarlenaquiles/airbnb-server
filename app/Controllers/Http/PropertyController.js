7f8bd10091a87ddf06d1f8ad36f23a08f50ae30e
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 18:50:26 -0300

    Trazendo as imagens no index

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -13,6 +13,7 @@ class PropertyController {
     const { latitude, longitude } = request.all()
 
     const properties = Property.query()
+      .with('images')
       .nearBy(latitude, longitude, 10000)
       .fetch()
 
