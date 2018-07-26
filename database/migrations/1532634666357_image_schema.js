683e0a533b0ff75b3093d245812054d065d35d45
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 16:53:51 -0300

    Criando os campos da table images

--- database/migrations/1532634666357_image_schema.js
+++ database/migrations/1532634666357_image_schema.js
@@ -6,6 +6,14 @@ class ImageSchema extends Schema {
   up () {
     this.create('images', (table) => {
       table.increments()
+      table
+        .integer('property_id')
+        .unsigned()
+        .references('id')
+        .inTable('properties')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('path').notNullable()
       table.timestamps()
     })
   }
