67fb2aa0158e8d3e3fc4d630bbeef9eb1a426b99
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 18:50:11 -0300

    Finalizando o método store

--- app/Controllers/Http/ImageController.js
+++ app/Controllers/Http/ImageController.js
@@ -2,10 +2,30 @@
 
 const Image = use('App/Models/Image')
 const Property = use('App/Models/Property')
+const Helpers = use('Helpers')
 
 class ImageController {
-    async store ({ request }) {
-        
+    async store({ request }) {
+        const property = await Property.findOrFail(params.id)
+
+        const images = request.file('image', {
+            types: ['image'],
+            size: '5mb'
+        })
+
+        await images.moveAll(Helpers.tmpPath('uploads'), file => ({
+            name: `${Date.now()}-${file.clientName}`
+        }))
+
+        if (!images.movedAll()) {
+            return images.errors()
+        }
+
+        await Promise.all(
+            images
+                .movedList()
+                .map(image => property.images().create({ path: image.fileName }))
+        )
     }
 }
 
